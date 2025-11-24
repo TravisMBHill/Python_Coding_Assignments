4490b7868d2b69b7a30922bc09b8fef0e788e045
Travis Hill <im.bradley04@gmail.com>
2025-11-23 21:42:11 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #welcome message to start
 print("Welcome to my Lego set purchase tracker!")
 Lego_sets = input("How many Lego sets have you purchased this year? ")
-Lego_sets = float(Lego_sets)
-total_spent = lego_sets * 49.99
-print(f"This year you've spent an estimated ${total_spent:.2f} on Lego sets!")
+#Change to float for calculation
+#put in try except block to catch non numeric inputs
 try:
     Lego_sets = float(Lego_sets)
     total_spent = Lego_sets * 49.99
 except ValueError:
     print("Please put a valid number for amount of Lego sets.")
     exit()
+#Print total spent
+print(f"This year you've spent an estimated ${total_spent:.2f} on Lego sets!")
