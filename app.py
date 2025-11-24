87692f8d199203eb471d0392c472c1d853f7ec2f
Travis Hill <im.bradley04@gmail.com>
2025-11-23 21:35:07 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -2,5 +2,11 @@
 print("Welcome to my Lego set purchase tracker!")
 Lego_sets = input("How many Lego sets have you purchased this year? ")
 Lego_sets = float(Lego_sets)
-total_sets = lego_sets * 49.99
+total_spent = lego_sets * 49.99
 print(f"This year you've spent an estimated ${total_spent:.2f} on Lego sets!")
+try:
+    Lego_sets = float(Lego_sets)
+    total_spent = Lego_sets * 49.99
+except ValueError:
+    print("Please put a valid number for amount of Lego sets.")
+    exit()
