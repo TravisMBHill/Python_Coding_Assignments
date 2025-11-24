69c4e67103cd774376e941eeb8d99152f36f68fc
Travis Hill <im.bradley04@gmail.com>
2025-11-23 21:30:49 -0500

    Add calculation for estimated LEGO spending

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 #welcome message to start
 print("Welcome to my Lego set purchase tracker!")
 Lego_sets = input("How many Lego sets have you purchased this year? ")
+Lego_sets = float(Lego_sets)
+total_sets = lego_sets * 49.99
