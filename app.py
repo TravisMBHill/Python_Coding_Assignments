330cbc86d0ae44a635d2f4b3e8bd5cb49369d138
Travis Hill <im.bradley04@gmail.com>
2025-11-23 21:32:04 -0500

    Added a formatted output displaying estimated Lego spending

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ print("Welcome to my Lego set purchase tracker!")
 Lego_sets = input("How many Lego sets have you purchased this year? ")
 Lego_sets = float(Lego_sets)
 total_sets = lego_sets * 49.99
+print(f"This year you've spent an estimated ${total_spent:.2f} on Lego sets!")
